70fc9effbbaaa6ef63e05a1a769aedc806e1115d
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-22 14:26:03 +0500

    handle fps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +19,8 @@ def main():
             
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
     
 if __name__ == "__main__":
     main()

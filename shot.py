3d416c419782a75d7f818c52a4aaa37e8385b37c
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-23 10:52:12 +0500

    fix: shots should vanish on impact with asteroids

--- shot.py
+++ shot.py
@@ -2,21 +2,16 @@ import pygame
 from constants import *
 from circleshape import CircleShape
 
+
 class Shot(CircleShape):
-    def __init__(self, x, y, SHOT_RADIUS):
-        pygame.sprite.Sprite.__init__(self)
-        CircleShape.__init__(self, x, y, SHOT_RADIUS)
-        self.radius = SHOT_RADIUS
-        self.velocity = pygame.Vector2(0,0)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
-        return pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2)
-    
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
     def update(self, dt):
         self.position += self.velocity * dt
 
-
-
-    
 
 

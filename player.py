06cc8c55b3ba129bf2762405776f5b8713218a0b
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-05-23 09:39:17 +0500

    feature: add shooting cooldown to player to prevent rapid fire

--- player.py
+++ player.py
@@ -9,6 +9,8 @@ class Player(CircleShape, pygame.sprite.Sprite):
         CircleShape.__init__(self, x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.shots = []
+        self.timer = 0
+
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -32,17 +34,18 @@ class Player(CircleShape, pygame.sprite.Sprite):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt 
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
 
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d]or keys[pygame.K_RIGHT]:
             self.rotate(dt)
 
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w]or keys[pygame.K_UP]:
             self.move(dt)
 
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s]or keys[pygame.K_DOWN]:
             self.move(-dt)
         
         if keys[pygame.K_SPACE]:
@@ -51,10 +54,12 @@ class Player(CircleShape, pygame.sprite.Sprite):
         for shot in self.shots:
             shot.update(dt)
 
-    def shoot(self):
-        direction = pygame.Vector2(0,1).rotate(self.rotation)
-        shot_position = self.position + direction * (self.radius + SHOT_RADIUS)
-        new_shot = Shot(shot_position.x, shot_position.y, SHOT_RADIUS)
-        new_shot.velocity = direction * PLAYER_SHOOT_SPEED
-        self.shots.append(new_shot)
 
+    def shoot(self):
+        if self.timer <= 0:
+            direction = pygame.Vector2(0,1).rotate(self.rotation)
+            shot_position = self.position + direction * (self.radius + SHOT_RADIUS)
+            new_shot = Shot(shot_position.x, shot_position.y, SHOT_RADIUS)
+            new_shot.velocity = direction * PLAYER_SHOOT_SPEED
+            self.shots.append(new_shot)
+            self.timer = PLAYER_SHOOT_COOLDOWN
